b09ca980779102dc9f440e5f585d27eb4cff57e0
Stas <nevmyvakastas@gmail.com>
2020-08-02 14:35:24 +0600

    Updates

---
+++ precache-manifest.beb2312ca83a649ac0423cc2a550133b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602b8a68fa8d004f2409c360f4660159",
+    "url": "/cartoon-quiz/index.html"
+  },
+  {
+    "revision": "535301cb71860adbbd46",
+    "url": "/cartoon-quiz/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "891f5df32366bb03d936",
+    "url": "/cartoon-quiz/static/js/2.265ac54a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cartoon-quiz/static/js/2.265ac54a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535301cb71860adbbd46",
+    "url": "/cartoon-quiz/static/js/main.58b13819.chunk.js"
+  },
+  {
+    "revision": "2ae1c074eae952b67fff",
+    "url": "/cartoon-quiz/static/js/runtime-main.532d3307.js"
+  },
+  {
+    "revision": "ec5b42615ac7234de7287580a4dcc0a4",
+    "url": "/cartoon-quiz/static/media/bg2.ec5b4261.png"
+  }
+]);
